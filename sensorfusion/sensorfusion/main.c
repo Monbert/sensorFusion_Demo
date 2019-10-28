61f1671b39ec74bf7f0af3e0487a36c2b2942316
Albert Xia <Albert@dhcp-134-129.cu-wireless-students.carleton.ca>
2019-10-28 18:52:49 -0400

    first commit for folder and file structure

---
+++ sensorfusion/sensorfusion/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sensorfusion
+//
+//  Created by Kai Xia on 28/10/2019.
+//  Copyright © 2019 carleton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
