59fe672868cfb7008de7f46c2b2c0e9659265dc0
Albert Xia <Albert@dhcp-134-129.cu-wireless-students.carleton.ca>
2019-10-30 21:53:15 -0400

    add data.c and algorithm.c

---
+++ sensorfusion/SensorFusionAlgorithm.c
@@ -0,0 +1,34 @@
+#Function of SensorFusion Algorithm
+
+
+void generateSDMatrix(float inputData[]){
+    float D[][];
+}
+
+void calcEigenvaluesAndEigenvectors(float D[][]){
+    float T[][];
+    float lambda[];
+}
+
+void calcPrincipalComponents(float D[][], float T[][]){
+    float y[][];
+}
+
+void calcContributionRate(float lambda[], float p){
+    float alpha[][];
+    int m;
+    float varphi_m;
+}
+
+void cmptISDScore(int m, float alpha[][], float y[][]){
+    float Z[][];
+}
+
+int eliminateWrongData(float Z[],int n){
+    return 0;
+    return 1;
+}
+
+void cmptFusedOutput(float Z[][], int n, float inputData[][]){
+    float outputData[][];
+}
---
+++ sensorfusion/sensorfusion/data.c
@@ -0,0 +1,15 @@
+#Function of DataInput or Output
+
+
+int readCsvData(char* csvFilePath){}
+
+void getCsvData(int index, float *){}
+
+int ifValueInsideRange(float inputData[][]){
+    return 0;
+    return 1;
+}
+
+void setOutputData(float outputData[][]){}
+
+int writeCsvData(){}
