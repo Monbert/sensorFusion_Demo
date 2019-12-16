449d640920322eecb657659bc53a1d33caa8b34c
KAI XIA <Albert@dhcp-129-136.cu-wireless-students.carleton.ca>
2019-12-15 20:35:58 -0500

    update sensor fusion algorithm core computing steps

--- src/algorithm.c
+++ src/algorithm.c
@@ -1,5 +1,7 @@
 /**
  * \brief Function Definition Library to Implement Sensor Fusion Algorithm
+ * \details this file implements the sensorfusion core computing steps, 
+ *    which we call sensorfusion algorithm
  * \author kaixia
  * \version 1.0
  * \date 2019-12-10
@@ -9,16 +11,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-#include "algorithm.h"
+#include "../include/algorithm.h"
 
 /**
- brief Function to initialize matrix
- details Initializing each new matrix when defined new matrix
- param[in] *matrix - it is a pointer to a struct of matrix
- param[in] row - it is a number of the row of the matrix
- param[in] colum - it is a number of the colum of the matrix
- return Success: true
- return Failure: false
+ * \brief Function to initialize matrix
+ * \details Initializing each new matrix when defined new matrix
+ * \param[in] *matrix - it is a pointer to a struct of matrix
+ * \param[in] row - it is a number of the row of the matrix
+ * \param[in] column - it is a number of the column of the matrix
+ * \return Success: true
+ * \return Failure: false
  */
 bool init_matrix(matrix *matrix, int row, int column){
     int matrix_size = row*column*sizeof(double);
@@ -39,22 +41,22 @@ bool init_matrix(matrix *matrix, int row, int column){
 }
 
 /**
- brief Function to get the size of a matrix
- details getting a size of a matrix
- param[in] *matrix - it is a pointer to a struct of matrix
- return Success: size of the matrix
- return Failure: NULL
+ * \brief Function to get the size of a matrix
+ * \details getting a size of a matrix
+ * \param[in] *matrix - it is a pointer to a struct of matrix
+ * \return Success: size of the matrix
+ * \return Failure: NULL
  */
 int get_matrix_size(matrix *matrix){
     return matrix->row*matrix->column;
 }
 
 /**
- brief Function to set matrix equal to zero
- details setting the every value in the matrix equal to zero
- param[in] *matrix - it is a pointer to a struct of matrix
- return Success: NULL
- return Failure: NULL
+ * \brief Function to set matrix equal to zero
+ * \details setting the every value in the matrix equal to zero
+ * \param[in] *matrix - it is a pointer to a struct of matrix
+ * \return Success: NULL
+ * \return Failure: NULL
  */
 void set_matrix_zeros(matrix *matrix){
     int matrix_num = get_matrix_size(matrix);
@@ -64,11 +66,11 @@ void set_matrix_zeros(matrix *matrix){
 }
 
 /**
- brief Function to tell if the matrix is null
- details telling if the matrix is null
- param[in] *matrix - it is a pointer to a struct of matrix
- return Success: true
- return Failure: false
+ * \brief Function to tell if the matrix is null
+ * \details telling if the matrix is null
+ * \param[in] *matrix - it is a pointer to a struct of matrix
+ * \return Success: true
+ * \return Failure: false
  */
 bool is_null_matrix(matrix *matrix){
     int matrix_num =get_matrix_size(matrix);
@@ -81,11 +83,11 @@ bool is_null_matrix(matrix *matrix){
 }
 
 /**
- brief Function to destroy the matrix
- details destroying matrix that will no longer use
- param[in] *matrix - it is a pointer to a struct of matrix
- return Success: NULL
- return Failure: NULL
+ * \brief Function to destroy the matrix
+ * \details destroying matrix that will no longer use
+ * \param[in] *matrix - it is a pointer to a struct of matrix
+ * \return Success: NULL
+ * \return Failure: NULL
  */
 void destroy_matrix(matrix *matrix){
     if (!is_null_matrix(matrix)){
@@ -97,29 +99,30 @@ void destroy_matrix(matrix *matrix){
 }
 
 /**
- brief Function to compute the second matrix norm
- details computing the second norm of the matrix
- param[in] *matrix - it is a pointer to a struct of matrix
- return Success: the value of the matrix norm
- return Failure: NULL
+ * \brief Function to compute the matrix norm
+ * \details computing the matrix norm and return this 
+ *    value to the function which needed
+ * \param[in] *matrix - it is a pointer to a struct of matrix
+ * \return Success: the value of the matrix norm
+ * \return Failure: NULL
  */
-double matrix_norm2(matrix *matrix){
-    double norm2_ans = 0.0;
+double get_matrix_norm(matrix *matrix){
+    double matrix_norm = 0.0;
     int matrix_num = get_matrix_size(matrix);
     for (int i = 0; i < matrix_num; i++){
-        norm2_ans+=(matrix->data[i]) * (matrix->data[i]);
+        matrix_norm+=(matrix->data[i]) * (matrix->data[i]);
     }
-    norm2_ans = (double)sqrt(norm2_ans);
-    return norm2_ans;
+    matrix_norm = (double)sqrt(matrix_norm);
+    return matrix_norm;
 }
 
 /**
- brief Function to copy the matrix
- details copying the value of matrix_a to the value of matrix_b
- param[in] *matrix_a - it is a pointer to a struct of matrix a
- param[in] *matrix_b - it is a pointer to a struct of matrix b
- return Success: NULL
- return Failure: NULL
+ * \brief Function to copy the matrix
+ * \details copying the value of matrix_a to the value of matrix_b
+ * \param[in] *matrix_a - it is a pointer to a struct of matrix a
+ * \param[in] *matrix_b - it is a pointer to a struct of matrix b
+ * \return Success: NULL
+ * \return Failure: NULL
  */
 void copy_matrix(matrix *matrix_a, matrix *matrix_b){
     if (matrix_b->row != matrix_a->row){
@@ -135,15 +138,19 @@ void copy_matrix(matrix *matrix_a, matrix *matrix_b){
 }
 
 /**
- brief Function to decompose the matrix
- details processing the matrix using QR-decomposition method, matrix_a is the original matirx
- param[in] *a - it is a pointer to a struct of matrix a
- param[in] *q - it is a pointer to a struct of matrix q
- param[in] *r - it is a pointer to a struct of matrix r
- return Success: NULL
- return Failure: NULL
+ * \brief Function to go through QR decomposition
+ * \details processing the matrix using QR decomposition method, 
+ *    matrix_a is the original matirx
+ * \param[in] *a - it is a pointer to a struct of matrix a, 
+ *    which is a original matrix
+ * \param[in] *q - it is a pointer to a struct of matrix q, 
+ *    which is an orthogonal matrix
+ * \param[in] *r - it is a pointer to a struct of matrix r, 
+ *    which is an upper triangular matrix
+ * \return Success: NULL
+ * \return Failure: NULL
  */
-void qr(matrix *a, matrix *q, matrix *r){
+void qr_decomposition(matrix *a, matrix *q, matrix *r){
     matrix col_a, col_q;
     init_matrix(&col_a, a->row, 1);
     set_matrix_zeros(&col_a);
@@ -183,13 +190,15 @@ void qr(matrix *a, matrix *q, matrix *r){
         for (int k = 0; k < j; k++){
             r->data[k * r->column + j] = 0;
             for(int i1 = 0; i1 < col_a.row; i1++){
-                r->data[k * r->column + j] += col_a.data[i1] * q->data[i1 * q->column + k];
+                r->data[k * r->column + j] += col_a.data[i1] * 
+                q->data[i1 * q->column + k];
             }
             for (int i2 = 0; i2 < a->column; i2++){
-                col_q.data[i2] -= r->data[k * r->column + j] * q->data[i2 * q->column + k];
+                col_q.data[i2] -= r->data[k * r->column + j] * 
+                q->data[i2 * q->column + k];
             }
         }
-        double temp = matrix_norm2(&col_q);
+        double temp = get_matrix_norm(&col_q);
         r->data[j * r->column + j] = temp;
         for (int i3 = 0; i3 < q->column; i3++){
             q->data[i3 * q->column + j] = col_q.data[i3] / temp;
@@ -200,12 +209,13 @@ void qr(matrix *a, matrix *q, matrix *r){
 }
 
 /**
- brief Function to sort the eigenvalues
- details sorting all eigenvalues, when falg is equal to 0 that means desc, it is equal to 1 that means asce
- param[in] *eigen_value - it is a pointer to a struct of matrix eigen_value
- param[in] flag - it is a flag to change the sorted order
- return Success: ture
- return Failure: false
+ * \brief Function to sort the eigenvalues
+ * \details sorting all eigenvalues, when flag is equal to 0 that means desc, 
+ *    it is equal to 1 that means asce
+ * \param[in] *eigen_value - it is a pointer to a struct of matrix eigen_value
+ * \param[in] flag - it is a flag to change the sorted order
+ * \return Success: ture
+ * \return Failure: false
  */
 bool sort_eigen_values(matrix *eigen_value, int flag){
     int size = get_matrix_size(eigen_value);
@@ -222,8 +232,9 @@ bool sort_eigen_values(matrix *eigen_value, int flag){
                     k = j;
                 }
             }
-            else
+            else{
                 return false;
+            }
         }
         if (k != i){
             double temp;
@@ -236,13 +247,14 @@ bool sort_eigen_values(matrix *eigen_value, int flag){
 }
 
 /**
- brief Function to multiply two matrixs
- details multiplying the values of two different matrixs a,b and getting the result matrix c
- param[in] *c - it is a pointer to a struct of matrix c
- param[in] *a - it is a pointer to a struct of matrix a
- param[in] *b - it is a pointer to a struct of matrix b
- return Success: ture
- return Failure: false
+ * \brief Function to multiply two matrixs
+ * \details multiplying the values of two different matrixs a,b and 
+ *    getting the result matrix c
+ * \param[in] *c - it is a pointer to a struct of matrix c
+ * \param[in] *a - it is a pointer to a struct of matrix a
+ * \param[in] *b - it is a pointer to a struct of matrix b
+ * \return Success: ture
+ * \return Failure: false
  */
 bool matrix_mul_matrix(matrix *c, matrix *a, matrix *b){
     if ((is_null_matrix(a)) || (is_null_matrix(b))){
@@ -260,7 +272,8 @@ bool matrix_mul_matrix(matrix *c, matrix *a, matrix *b){
         for (int j = 0; j < b->column; j++)
         {
             for (int k = 0; k < a->column; k++){
-                c->data[i*c->row + j] += a->data[i*a->row + k] * b->data[k*b->column + j];
+                c->data[i*c->row + j] += a->data[i*a->row + k] * 
+                b->data[k*b->column + j];
             }
         }
     }
@@ -268,13 +281,14 @@ bool matrix_mul_matrix(matrix *c, matrix *a, matrix *b){
 }
 
 /**
- brief Function to comput the eigenvectors
- details calculating all eigenvetors through all eigenvalues we got before, and storing in matrix a
- param[in] *a - it is a pointer to a struct of matrix a
- param[in] *eigen_vector - it is a pointer to a struct of matrix eigen_vector
- param[in] *eigen_values - it is a pointer to a struct of matrix eigen_values
- return Success: NULL
- return Failure: NULL
+ * \brief Function to comput the eigenvectors
+ * \details calculating all eigenvetors through all eigenvalues we got before, 
+ *    and storing in matrix a
+ * \param[in] *a - it is a pointer to a struct of matrix a
+ * \param[in] *eigen_vector - it is a pointer to a struct of matrix eigen_vector
+ * \param[in] *eigen_values - it is a pointer to a struct of matrix eigen_values
+ * \return Success: NULL
+ * \return Failure: NULL
  */
 void calculate_eigenvector(matrix *a, matrix *eigen_vector, matrix *eigen_values){
     int num = a->column;
@@ -295,15 +309,18 @@ void calculate_eigenvector(matrix *a, matrix *eigen_vector, matrix *eigen_values
             for (int i1 = i + 1; i1 < temp.row; i1++){
                 coe = temp.data[i1 * temp.column + i];
                 for (int j1 = i; j1 < temp.column; j1++){
-                    temp.data[i1 * temp.column + j1] -= coe * temp.data[i * temp.column + j1];
+                    temp.data[i1 * temp.column + j1] -= coe * 
+                    temp.data[i * temp.column + j1];
                 }
             }
         }
-        double sum1 = eigen_vector->data[(eigen_vector->row - 1) * eigen_vector->column + count] = 1;
+        double sum1 = eigen_vector->data[(eigen_vector->row - 1) * 
+            eigen_vector->column + count] = 1;
         for (int i2 = temp.row - 2; i2 >= 0; i2--){
             double sum2 = 0;
             for (int j2 = i2 + 1; j2 < temp.column; j2++){
-                sum2 += temp.data[i2 * temp.column + j2] * eigen_vector->data[j2 * eigen_vector->column + count];
+                sum2 += temp.data[i2 * temp.column + j2] * 
+                eigen_vector->data[j2 * eigen_vector->column + count];
             }
             sum2 = -sum2 / temp.data[i2 * temp.column + i2];
             sum1 += sum2 * sum2;
@@ -319,12 +336,13 @@ void calculate_eigenvector(matrix *a, matrix *eigen_vector, matrix *eigen_values
 }
 
 /**
- brief Function to get support degree matrix
- details calculating the support degree of every two sensors and getting the support degree matrix
- param[in] sensor - it is a struct of storing all sensor data
- param[in] n - it is a number of all sensors
- return Success: a pointer to a two dimentional array of support degree matrix
- return Failure: NULL
+ * \brief Function to get support degree matrix
+ * \details calculating the support degree of every two sensors and 
+ *    getting the support degree matrix
+ * \param[in] sensor - it is a struct of storing all sensor data
+ * \param[in] n - it is a number of all sensors
+ * \return Success: a pointer to a two dimentional array of support degree matrix
+ * \return Failure: NULL
  */
 double **calculate_sd_matrix(struct sensor_list sensor[],int n){
     int i, j;
@@ -348,11 +366,11 @@ double **calculate_sd_matrix(struct sensor_list sensor[],int n){
 }
 
 /**
- brief Function to transpose eigenvectors
- details generating all transposed eigenvectors
- param[in] *matrix - it is a pointer to a struct of matrix matrix
- return Success: a pointer to a two dimentional array of d
- return Failure: NULL
+ * \brief Function to transpose eigenvectors
+ * \details generating all transposed eigenvectors
+ * \param[in] *matrix - it is a pointer to a struct of matrix matrix
+ * \return Success: a pointer to a two dimentional array of d
+ * \return Failure: NULL
  */
 double **generate_eigenvector_transpose(matrix *matrix){
     int i,j;
@@ -371,14 +389,16 @@ double **generate_eigenvector_transpose(matrix *matrix){
 }
 
 /**
- brief Function to calculate the value of alpha_k
- details generating an array of all values of contribution rate of kth principal component
- param[in] *matrix - it is a pointer to a struct of matrix matrix
- param[in] n - it is a number of all sensors
- return Success: a pointer to an array of aplha_k
- return Failure: NULL
+ * \brief Function to calculate the value of alpha_k
+ * \details generating an array of all values of contribution rate of 
+ *    kth principal component
+ * \param[in] *matrix - it is a pointer to a struct of matrix matrix
+ * \param[in] n - it is a number of all sensors
+ * \return Success: a pointer to an array of aplha_k
+ * \return Failure: NULL
  */
-double *calculate_contribution_rate_of_kth_principal_component(matrix *matrix, int n){
+double *calculate_contribution_rate_of_kth_principal_component(
+    matrix *matrix, int n){
     double *alpha_k;
     double sum = 0.0;
     alpha_k = (double*)malloc(sizeof(double)*n);
@@ -392,16 +412,18 @@ double *calculate_contribution_rate_of_kth_principal_component(matrix *matrix, i
 }
 
 /**
- brief Function to calculate the value of z
- details computing an array of all values of integrated support degree score
- param[in] *matrix - it is a pointer to a struct of matrix matrix
- param[in] *alpha_k - it is a pointer to an array of aplha_k
- param[in] n - it is a number of all sensors
- param[in] m - it is a value that means we will select the first m principal components later
- return Success: a pointer to an array of integrated support degree score
- return Failure: NULL
+ * \brief Function to calculate the value of z
+ * \details computing an array of all values of integrated support degree score
+ * \param[in] *matrix - it is a pointer to a struct of matrix matrix
+ * \param[in] *alpha_k - it is a pointer to an array of aplha_k
+ * \param[in] n - it is a number of all sensors
+ * \param[in] m - it is a value that means we will select the first 
+ *    m principal components later
+ * \return Success: a pointer to an array of integrated support degree score
+ * \return Failure: NULL
  */
-double *compute_integrated_support_degree_score(matrix *y,double *alpha_k,int n,int m){
+double *compute_integrated_support_degree_score(
+    matrix *y,double *alpha_k,int n,int m){
     double *z;
     int i,j;
     z = (double*)malloc(sizeof(double)*n);
@@ -414,13 +436,15 @@ double *compute_integrated_support_degree_score(matrix *y,double *alpha_k,int n,
 }
 
 /**
- brief Function to disregard wrong data
- details disregarding invalid data, setting the invalid sensor equal to 0.0 and getting an array of new z after disregarding
- param[in] *z - it is a pointer to an array of all values of integrated support degree score
- param[in] n - it is a number of all sensors
- param[in] q - it is a changeable value that can limit the valid data
- return Success: a pointer to an array of new integrated support degree score after disgarding wrong sensor data
- return Failure: NULL
+ * \brief Function to disregard wrong data
+ * \details disregarding invalid data, setting the invalid sensor equal to 0.0 and 
+ *    getting an array of new z after disregarding
+ * \param[in] *z - it is a pointer to an array of all values of integrated support degree score
+ * \param[in] n - it is a number of all sensors
+ * \param[in] q - it is a changeable value that can limit the valid data
+ * \return Success: a pointer to an array of new integrated support degree score 
+ *    after disgarding wrong sensor data
+ * \return Failure: NULL
  */
 double *disregard_wrong_data(double *z,int n,double q){
     int i;
@@ -443,12 +467,14 @@ double *disregard_wrong_data(double *z,int n,double q){
 }
 
 /**
- brief Function to compute weight coefficient
- details computing the weight coefficient of every valid sensor and getting an array of weight
- param[in] *z_after - it is a pointer to an array new integrated support degree score after disgarding wrong sensor data
- param[in] n - it is a number of all sensors
- return Success: a pointer to an array of weight coefficient of every valid sensor
- return Failure: NULL
+ * \brief Function to compute weight coefficient
+ * \details computing the weight coefficient of every valid sensor and 
+ *    getting an array of weight
+ * \param[in] *z_after - it is a pointer to an array 
+ *    new integrated support degree score after disgarding wrong sensor data
+ * \param[in] n - it is a number of all sensors
+ * \return Success: a pointer to an array of weight coefficient of every valid sensor
+ * \return Failure: NULL
  */
 double *compute_weight_coefficient(double *z_after,int n){
     double *weight;
@@ -467,13 +493,14 @@ double *compute_weight_coefficient(double *z_after,int n){
 }
 
 /**
- brief Function to compute fused output
- details getting the value of fused output as the fianl result
- param[in] sensor - it is a struct of storing all sensor data
- param[in] *weight - it is a pointer to an array of weight coefficient of every valid sensor
- param[in] n - it is a number of all sensors
- return Success: the value of fused output as the fianl result
- return Failure: NULL
+ * \brief Function to compute fused output
+ * \details getting the value of fused output as the fianl result
+ * \param[in] sensor - it is a struct of storing all sensor data
+ * \param[in] *weight - it is a pointer to an array of 
+ *    weight coefficient of every valid sensor
+ * \param[in] n - it is a number of all sensors
+ * \return Success: the value of fused output as the fianl result
+ * \return Failure: NULL
  */
 double compute_fused_output(struct sensor_list sensor[],double *weight,int n){
     double x_output=0.0;
